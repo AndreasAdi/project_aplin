502783d3f2dd77ff2afda48ea675cc2827db32f7
Windows <enrico1@mhs.stts.edu>
2020-04-21 14:52:18 +0700

    asd

---
+++ LoginSuccess.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>BioskopID</title>
+  </head>
+  <body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  
+
+
+    
+
+ 
+    <!--NAVBAR-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="Index.php">BIOSKOPID</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="List_Film.php">List Film</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Meals.php">Pre-Order Snack</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Ticketing.php">Ticketing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Riwayat.php">Riwayat</a>
+            </li>
+            </ul>
+
+     <!-- <a href="index.php"> <h3 style="text-align:center; float:left; margin-left: 47%;">BioskopID</h3> </a> -->
+    <a href="Register.php"> <text class="nav-link text-primary mr-2">Sign Up</text> </a>
+    <a href="Ticketing.php"> <text class="nav-link text-secondary">Login</text> </a>
+        </div>
+        </nav>
+
+    <!-- ISIAN PERTAMA-->
+    <div class="container-fluid mt-5">
+        <div class='badge badge-primary text-wrap bg-primary text-light' style='width:100%;height:100%;'>
+            <p class='font-weight-bold' style='font-size:30px;'>Login Berhasil!</p> <br>
+            <p class='font-weight-bold' style='font-size:20px;'>Selamat Datang,User!</p>
+        </div>
+    </div>
+        <!--FOOTER-->
+        <footer class="page-footer font-small fixed-bottom bg-dark text-light mt-5">
+            <div class="footer-copyright text-center py-3">© 2020 Copyright
+            </div>
+        </footer>
+  </body>
+</html>
