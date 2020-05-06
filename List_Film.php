787ff776157676d8e80e513c4f9259086e02fc36
Calista368 <Calistaa368@users.noreply.github.com>
2020-05-06 22:19:11 +0700

    list

--- List_Film.php
+++ List_Film.php
@@ -34,7 +34,7 @@ if(isset($_POST['btnbook'])){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>List Film!</title>
   </head>
   <body>
     <!-- Optional JavaScript -->
