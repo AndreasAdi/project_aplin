5b37bef34dfc007720b01ccb8d8878cdcc597f35
Andreas Adi <andreas1@mhs.stts.edu>
2020-05-15 13:35:22 +0700

    judul

--- Detail_film.php
+++ Detail_film.php
@@ -47,7 +47,7 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <link rel="stylesheet" href="detailfilm.css">
-    <title>Hello, world!</title>
+    <title><?php echo $result['judul']?></title>
 </head>
 
 <body>
